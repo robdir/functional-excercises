67e7ab12bda8fec9a41649996e934c9459029574
robdir <rhwickham@gmail.com>
2018-01-05 13:30:28 +0100

    reduce completed

--- reducechallenge.js
+++ reducechallenge.js
@@ -1,3 +1,5 @@
+// my solution
+
 function countWords(inputWords) {
    return inputWords.reduce(function (arr, el) {
         if (el in arr) {
@@ -11,3 +13,12 @@ function countWords(inputWords) {
 };
 
 module.exports = countWords
+
+// their solution
+
+function countSomeWords(arr) {
+    return arr.reduce(function(countMap, word) {
+        countMap[word] = ++countMap[word] || 1 // increment or initialise to 1
+        return countMap 
+    }, {}) // second argument of reduce initialises countMap to 1
+}
