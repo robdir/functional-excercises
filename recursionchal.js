d3c125d6f3b6decd1f3b305180315dcb5f79be83
robdir <rhwickham@gmail.com>
2018-02-01 14:08:33 +0100

    recursion challenge completed

--- recursionchal.js
+++ recursionchal.js
@@ -1,5 +1,10 @@
 function reduce(arr, fn, initial) {
-    
+    return (function reduceOnce(curr, prev) {
+        if ( curr >= arr.length ){
+            return prev;
+        }
+        return reduceOnce(curr + 1, fn(prev, arr[curr], curr, arr));
+    } (0, initial));
 }
 
 module.exports = reduce
