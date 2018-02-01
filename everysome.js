129af4a520cac1d3b4435ac96aaadcc5621718fb
robdir <rhwickham@gmail.com>
2018-02-01 14:08:01 +0100

    everysome correct solution

--- everysome.js
+++ everysome.js
@@ -1,11 +1,11 @@
 function checkUsersValid(goodUsers) {
     return function allUsersValid(submittedUsers) {
-        return submittedUsers.every(function (user) {
+        return submittedUsers.every(function (user) { // each user must be checked against 
             return goodUsers.some(function (validUser){
                 return user.id === validUser.id
             })
         })
     };
-};
+}
 
 module.exports  = checkUsersValid
