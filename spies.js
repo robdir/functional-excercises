f3dcd2dc4a78aab7d66791e61510ed2d7022f9ff
robdir <rhwickham@gmail.com>
2018-02-02 11:56:26 +0100

    spies completed

--- spies.js
+++ spies.js
@@ -1,3 +1,15 @@
-Spy = (target, method) => {
-    
+function Spy(target, method) {
+    var proxy = target[method];
+    var results = {
+        count: 0
+    };
+
+    target[method] = function () {
+        results.count += 1;
+        return proxy.apply(this, arguments);
+    };
+
+    return results;
 }
+
+module.exports = Spy;
